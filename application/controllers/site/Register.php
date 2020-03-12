13c1605f7065fc826ccb1c1cf01716b0e0487cd2
T3RON <seif4847@gmail.com>
2020-03-12 16:20:27 +0330

    1398/12/22

--- application/controllers/site/Register.php
+++ application/controllers/site/Register.php
@@ -6,18 +6,26 @@
  * Time: 04:13 AM
  */
 
-class Register extends MY_Controller {
+class Register extends CI_Controller {
     function __construct()
     {
         parent::__construct();
         $this->load->model('Menu_Model');
         $this->load->model('MY_Model');
+        $this->load->library('ion_auth');
+        $this->load->library('Jdf');
+        $this->load->library('user_agent');
+        $this->load->helper('url');
+        $this->load->helper('form');
+
     }
 
     function index()
     {
         $output['menu_top'] = $this->Menu_Model->select('menu');
         $output['slider'] = $this->Menu_Model->select('slider');
+        $output['menu_middel'] = $this->Menu_Model->select('secend_menu');
+        $output['footer_menu'] = $this->Menu_Model->select('footer_menu');
         $output['text'] = $this->Menu_Model->select('text');
         $output['site'] = $this->MY_Model->select_single('site',1);
 
@@ -28,6 +36,10 @@ class Register extends MY_Controller {
         $output['des'] = "مديريت و بررسي كاربران";
         $output['timeStamp'] = $this->jdf->jdate('l, j F Y',time(),'','GMT');
         $this->load->view('site/Register', $output);
+    }
+
+    function register() {
+        
     }
 
 
