0501b55384aafdc80d575230387686c8a92cbc5d
MohammadSeif <seif4847@gmail.com>
2020-08-31 12:32:57 +0430

    newww

--- New folder/dist/js/myjs.js
+++
@@ -1,9 +0,0 @@
-var select_movies = document.getElementById("movies_select");
-multi(select_movies, {
-    enable_search: true
-});
-
-var select_series = document.getElementById("series_select");
-multi(select_series, {
-    enable_search: true
-});
