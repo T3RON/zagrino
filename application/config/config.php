a5b534562ccb1296eaa18d73eb607852e44e42ea
Mseif <seif4847@gmail.com>
2020-10-15 11:31:45 +0330

    1399/07/24

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://zagrino.nahavandflour.com/';
+$config['base_url'] = 'http://localhost/zagrino/';
 $config['thekey']='XhWQgd2YzM1CN1QEZQ8Q2uCBNqR2FxEt';
 
 /*
